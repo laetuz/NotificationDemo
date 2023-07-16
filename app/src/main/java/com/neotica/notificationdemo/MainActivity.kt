45a23514d700376ef4c1ab2f527a41f518ecd652
Ryo Martin <laetuzg@gmail.com>
2023-07-16 19:58:18 +0700

    feat: Added notificationManager logic inside main activity and added a button in the main activity layout to trigger the notification.

--- app/src/main/java/com/neotica/notificationdemo/MainActivity.kt
+++ app/src/main/java/com/neotica/notificationdemo/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.neotica.notificationdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import com.neotica.notificationdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +16,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    fun sendNotification(view: View){
+        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.baseline_notifications_24)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.baseline_notifications_24))
+            .setContentTitle(resources.getString(R.string.content_title))
+            .setContentText(resources.getString(R.string.content_text))
+            .setSubText(resources.getString(R.string.subtext))
+            .setAutoCancel(true)
+
+        /* Create or update. */
+        val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+        channel.description = CHANNEL_NAME
+        mBuilder.setChannelId(CHANNEL_ID)
+        mNotificationManager.createNotificationChannel(channel)
+
+        val notification = mBuilder.build()
+        mNotificationManager.notify(NOTIFICATION_ID, notification)
+    }
+
+    companion object{
+        private const val NOTIFICATION_ID = 1
+        private const val CHANNEL_ID = "channel_01"
+        private const val CHANNEL_NAME = "neotica channel"
     }
 }
